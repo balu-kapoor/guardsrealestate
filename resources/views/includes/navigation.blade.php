8505d194c838672a4eaa31dc39a9ef6f37e2c84c
balu-kapoor <brkapoor11@gmail.com>
2022-09-16 14:39:05 +0530

    adding files

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -31,7 +31,7 @@
                         <li style=" -webkit-animation-duration: 1.1s;animation-duration: 1.1s;"><a href="/valuation/online" >Valuations</a></li>
                         <li style=" -webkit-animation-duration: 1.2s;animation-duration: 1.2s;">
                         <li style=" -webkit-animation-duration: 1.6s;animation-duration: 1.6s;"><a href="{{route('repair-maintenance')}}">Repair And Maintenance</a></li>
-                        <li style=" -webkit-animation-duration: 1.6s;animation-duration:.5s;">
+                        <li style=" -webkit-animation-duration: 1.6s;animation-duration: 1.6s;">
                             <div class="collapsible-header"><i class="material-icons">add</i>About Us</div>
                             <ul class="collapsible-body">
                                 <li><a  href="{{route('website-about')}}">About Us</a></li>
