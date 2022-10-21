1d4e29020ae9413638e381b19a695514c25e63f0
balu-kapoor <brkapoor11@gmail.com>
2022-10-21 19:48:36 +0530

    changing numbers

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -8,7 +8,7 @@
                         <a href="tel:+442036331271" class="m-text" style="line-height: 1rem;display: block">+44 (0) 20 3633 1271</a>
                         <span>Guards London</span>
                         <a href="tel:+44001642687188" class="m-text" style="line-height: 1rem;display: block">+44 (0) 01642 687 188</a>
-                        <span>Guards North Yorkshire</span>
+                        <span>Guards Country</span>
                     </div>
                     <ul class="collapsible">
                         <li style=" -webkit-animation-duration: .5s;animation-duration:.5s;"><a href="{{route('website-home')}}" class="selected-nav ">Home</a>
